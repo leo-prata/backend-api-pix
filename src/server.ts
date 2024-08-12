9d111f372553ad5cc06fa5d20211f2486d1688c9
leo-prata <leo.prata@estudante.ufjf.br>
2024-08-12 17:35:38 -0300

    add server template

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { router } from './routes';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.use(
+    '/files', 
+    express.static(path.resolve(__dirname, '..', 'tmp'))
+);
+
+app.use((e: Error, req: Request, res: Response, next: NextFunction) => {
+    if(e instanceof Error){
+        return res.status(400).json({
+            error: e.message
+        });
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'internal server error'
+    })
+});
+
+
+app.listen(3111, () => console.log('Server is running on port 3111'));
