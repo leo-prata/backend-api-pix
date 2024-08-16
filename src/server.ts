2a25d43cffc0a1924e01248eb4a03e137b415e76
leo-prata <leo.prata@estudante.ufjf.br>
2024-08-16 12:20:32 -0300

    arruma server cors

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,7 @@ import path from 'path';
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: process.env.API_VERCEL,
-    methods: ['GET, POST, PUT, DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(router);
 
